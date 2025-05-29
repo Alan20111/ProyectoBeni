b72a81022b111aee14bccbb27aac956e7ec135af
spongeenee <24031115@itcelaya.edu.mx>
2025-05-28 22:26:02 -0600

    Mierdas

---
+++ src/inicioarchivos/betatester/BetaTester.java
@@ -0,0 +1,28 @@
+package inicioarchivos.betatester;
+
+/**
+ *
+ * @author markb
+ */
+
+import javax.swing.*;
+public class BetaTester 
+{
+    JFrame ven;
+    public BetaTester(){
+        initComponents();
+    }
+    
+    public void initComponents()
+    {
+        ven = new JFrame();
+        ven.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ven.setAlwaysOnTop(true);
+        ven.setSize(300,300);
+        ven.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        BetaTester bt = new BetaTester();
+    }
+}
