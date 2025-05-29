f1bbd0fb9079f35c5b0c2ff732767f1fccf44abe
spongeenee <24031115@itcelaya.edu.mx>
2025-05-28 23:38:01 -0600

    Modificar 2

--- src/inicioarchivos/ModificarVen.java
+++ src/inicioarchivos/ModificarVen.java
@@ -4,8 +4,10 @@ package inicioarchivos;
  * @author markb
  */
 
+import java.awt.*;
+import javax.swing.*;
 import inicioarchivos.Ventana;
-public class ModificarVen extends javax.swing.JFrame 
+public class ModificarVen extends JFrame
 {
     public ModificarVen() 
     {
@@ -18,122 +20,113 @@ public class ModificarVen extends javax.swing.JFrame
 
         filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(10, 0), new java.awt.Dimension(10, 0), new java.awt.Dimension(10, 32767));
         filler3 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 10), new java.awt.Dimension(0, 10), new java.awt.Dimension(32767, 10));
-        jPanel1 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
-        jPanel5 = new javax.swing.JPanel();
+        pnlTODO = new javax.swing.JPanel();
+        pnlData = new javax.swing.JPanel();
+        pnlA = new javax.swing.JPanel();
         lblA = new javax.swing.JLabel();
         txtDatoA = new javax.swing.JTextField();
-        jPanel6 = new javax.swing.JPanel();
+        pnlB = new javax.swing.JPanel();
         lblB = new javax.swing.JLabel();
-        txtDatoA1 = new javax.swing.JTextField();
-        jPanel7 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        txtDatoA2 = new javax.swing.JTextField();
-        jPanel2 = new javax.swing.JPanel();
+        txtDatoB = new javax.swing.JTextField();
+        pnlC = new javax.swing.JPanel();
+        lblC = new javax.swing.JLabel();
+        txtDatoC = new javax.swing.JTextField();
+        pnlOpciones = new javax.swing.JPanel();
         btnCancelar = new javax.swing.JButton();
         btnModificar = new javax.swing.JButton();
         filler2 = new javax.swing.Box.Filler(new java.awt.Dimension(10, 0), new java.awt.Dimension(10, 0), new java.awt.Dimension(10, 32767));
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Modificar");
+        setResizable(false);
         getContentPane().add(filler1, java.awt.BorderLayout.WEST);
         getContentPane().add(filler3, java.awt.BorderLayout.SOUTH);
 
-        jPanel1.setLayout(new java.awt.BorderLayout(10, 0));
+        pnlTODO.setLayout(new java.awt.BorderLayout(10, 0));
 
-        jPanel4.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+        pnlData.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
-        jLabel1.setText("Modificar Alumno");
-        jPanel4.add(jLabel1);
+        pnlA.setAlignmentX(0.0F);
+        pnlA.setAlignmentY(0.0F);
+        pnlA.setLayout(new javax.swing.BoxLayout(pnlA, javax.swing.BoxLayout.PAGE_AXIS));
 
-        jPanel1.add(jPanel4, java.awt.BorderLayout.NORTH);
-
-        jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-
-        jPanel5.setAlignmentX(0.0F);
-        jPanel5.setAlignmentY(0.0F);
-        jPanel5.setLayout(new javax.swing.BoxLayout(jPanel5, javax.swing.BoxLayout.PAGE_AXIS));
-
+        lblA.setText("jLabel2");
         lblA.setAlignmentY(0.0F);
-        jPanel5.add(lblA);
+        pnlA.add(lblA);
 
         txtDatoA.setColumns(8);
+        txtDatoA.setText("jTextField1");
         txtDatoA.setAlignmentX(0.0F);
         txtDatoA.setAlignmentY(0.0F);
-        txtDatoA.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtDatoAActionPerformed(evt);
-            }
-        });
-        jPanel5.add(txtDatoA);
+        pnlA.add(txtDatoA);
 
-        jPanel3.add(jPanel5);
+        pnlData.add(pnlA);
 
-        jPanel6.setLayout(new javax.swing.BoxLayout(jPanel6, javax.swing.BoxLayout.PAGE_AXIS));
+        pnlB.setLayout(new javax.swing.BoxLayout(pnlB, javax.swing.BoxLayout.PAGE_AXIS));
 
+        lblB.setText("jLabel2");
         lblB.setAlignmentY(0.0F);
-        jPanel6.add(lblB);
+        pnlB.add(lblB);
 
-        txtDatoA1.setColumns(35);
-        txtDatoA1.setAlignmentX(0.0F);
-        txtDatoA1.setAlignmentY(0.0F);
-        txtDatoA1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtDatoA1ActionPerformed(evt);
-            }
-        });
-        jPanel6.add(txtDatoA1);
+        txtDatoB.setColumns(35);
+        txtDatoB.setText("jTextField1");
+        txtDatoB.setAlignmentX(0.0F);
+        txtDatoB.setAlignmentY(0.0F);
+        pnlB.add(txtDatoB);
 
-        jPanel3.add(jPanel6);
+        pnlData.add(pnlB);
 
-        jPanel7.setLayout(new javax.swing.BoxLayout(jPanel7, javax.swing.BoxLayout.PAGE_AXIS));
+        pnlC.setLayout(new javax.swing.BoxLayout(pnlC, javax.swing.BoxLayout.PAGE_AXIS));
 
-        jLabel4.setAlignmentY(0.0F);
-        jPanel7.add(jLabel4);
+        lblC.setText("jLabel2");
+        lblC.setAlignmentY(0.0F);
+        pnlC.add(lblC);
 
-        txtDatoA2.setColumns(2);
-        txtDatoA2.setAlignmentX(0.0F);
-        txtDatoA2.setAlignmentY(0.0F);
-        txtDatoA2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtDatoA2ActionPerformed(evt);
-            }
-        });
-        jPanel7.add(txtDatoA2);
+        txtDatoC.setColumns(2);
+        txtDatoC.setText("jTextField1");
+        txtDatoC.setAlignmentX(0.0F);
+        txtDatoC.setAlignmentY(0.0F);
+        pnlC.add(txtDatoC);
 
-        jPanel3.add(jPanel7);
+        pnlData.add(pnlC);
 
-        jPanel1.add(jPanel3, java.awt.BorderLayout.CENTER);
+        pnlTODO.add(pnlData, java.awt.BorderLayout.CENTER);
 
         btnCancelar.setText("Cancelar");
-        jPanel2.add(btnCancelar);
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelar(evt);
+            }
+        });
+        pnlOpciones.add(btnCancelar);
 
         btnModificar.setText("Modificar");
-        jPanel2.add(btnModificar);
+        btnModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                modificar(evt);
+            }
+        });
+        pnlOpciones.add(btnModificar);
 
-        jPanel1.add(jPanel2, java.awt.BorderLayout.SOUTH);
+        pnlTODO.add(pnlOpciones, java.awt.BorderLayout.SOUTH);
 
-        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+        getContentPane().add(pnlTODO, java.awt.BorderLayout.CENTER);
         getContentPane().add(filler2, java.awt.BorderLayout.EAST);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
     private void moreComponents(){
-        
+
     }
-    private void txtDatoAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDatoAActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtDatoAActionPerformed
+    private void cancelar(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelar
+        dispose();
+    }//GEN-LAST:event_cancelar
 
-    private void txtDatoA1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDatoA1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtDatoA1ActionPerformed
-
-    private void txtDatoA2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDatoA2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtDatoA2ActionPerformed
+    private void modificar(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_modificar
+        String text;
+        text=String.format("%8s | %-40s | %2d", txtDatoA.getText(), txtDatoB.getText(), Byte.valueOf(txtDatoC.getText()));
+        JOptionPane.showMessageDialog(rootPane, "Todavía no está listo\n"+text);
+    }//GEN-LAST:event_modificar
 
     public static void main(String args[]) {
         ModificarVen mv = new ModificarVen();
@@ -146,19 +139,17 @@ public class ModificarVen extends javax.swing.JFrame
     private javax.swing.Box.Filler filler1;
     private javax.swing.Box.Filler filler2;
     private javax.swing.Box.Filler filler3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
     private javax.swing.JLabel lblA;
     private javax.swing.JLabel lblB;
+    private javax.swing.JLabel lblC;
+    private javax.swing.JPanel pnlA;
+    private javax.swing.JPanel pnlB;
+    private javax.swing.JPanel pnlC;
+    private javax.swing.JPanel pnlData;
+    private javax.swing.JPanel pnlOpciones;
+    private javax.swing.JPanel pnlTODO;
     private javax.swing.JTextField txtDatoA;
-    private javax.swing.JTextField txtDatoA1;
-    private javax.swing.JTextField txtDatoA2;
+    private javax.swing.JTextField txtDatoB;
+    private javax.swing.JTextField txtDatoC;
     // End of variables declaration//GEN-END:variables
 }
